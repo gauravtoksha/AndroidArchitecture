21d375b916099fc785c952371c48310fcca89c2d
sanket mali <malisanket11@gmail.com>
2019-04-17 14:25:10 +0530

    added method to get device height,width in dpi changed getDeviceHeightPixels() and getDeviceHeightPixels() implementation.

--- app/src/main/java/com/example/myapplication/util/AppUtility.java
+++ app/src/main/java/com/example/myapplication/util/AppUtility.java
@@ -1,9 +1,11 @@
 package com.example.myapplication.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Point;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
 
@@ -14,7 +16,8 @@ import java.util.Date;
 import java.util.Locale;
 
 public class AppUtility {
-    private static Point size=new Point();
+
+    static DisplayMetrics metrics = new DisplayMetrics();
 
     public static int getDeviceVersion(){
         int currentVer = android.os.Build.VERSION.SDK_INT;
@@ -22,31 +25,28 @@ public class AppUtility {
     }
 
     //get device height and width in pixels
-    public static int getDeviceWidth(Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-//        int width = display.getWidth(); depricated
-        display.getSize(size);
-        return size.x;
-    }
-
-    public static int getDeviceHeight(Context context) {
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        Display display = wm.getDefaultDisplay();
-        display.getSize(size);
-        return size.y;
-    }
-
-    /*
-    * // pixels, dpi
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    public static int getDeviceWidthPixels(Activity activity) {
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         int heightPixels = metrics.heightPixels;
+        return heightPixels;
+    }
+
+    public static int getDeviceHeightPixels(Activity activity) {
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         int widthPixels = metrics.widthPixels;
-        int densityDpi = metrics.densityDpi;
+        return widthPixels;
+    }
+    public static float getDeviceHeightDpi(Activity activity) {
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
         float xdpi = metrics.xdpi;
-        float ydpi = metrics.ydpi;
-    * */
+        return xdpi;
+    }
+
+    public static float getDeviceWidthDpi(Activity activity) {
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int widthPixels = metrics.widthPixels;
+        return widthPixels;
+    }
 
     public static boolean isInternetAvailable(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
