ac122eb48823d98cd72f89db0234909cc0706179
sanket mali <malisanket11@gmail.com>
2019-04-17 15:46:00 +0530

    added method to get device height,width in dpi changed getDeviceHeightPixels() and getDeviceHeightPixels() implementation.

---
+++ app/src/main/java/com/example/myapplication/binding/FragmentBindingAdapters.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.binding;
+
+import android.databinding.BindingAdapter;
+import android.support.v4.app.Fragment;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import javax.inject.Inject;
+
+public class FragmentBindingAdapters {
+    final Fragment fragment;
+
+    @Inject
+    public FragmentBindingAdapters(Fragment fragment) {
+        this.fragment = fragment;
+    }
+//    @BindingAdapter("imageUrl")
+//    public void bindImage(ImageView imageView, String url) {
+//        Glide.with(fragment).load(url).into(imageView);
+//    }
+}
